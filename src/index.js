ff5c2e74090c0f0e5e7b0014dc5f28a0e2a541bb
Kenny Lee <kenny.lee927@gmail.com>
2022-07-16 10:58:36 -0400

    add image using webpack

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './styles/style.css';
+import logo from './images/logo.png';
 console.log("test");
+
+const content = document.getElementById('content');
+const mainLogo = new Image();
+mainLogo.src = logo;
+content.appendChild(mainLogo);
