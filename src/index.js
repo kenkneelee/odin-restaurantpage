91368f9e26bba778c7aeb1867e456641b5ea5606
Kenny Lee <kenny.lee927@gmail.com>
2022-07-18 18:21:25 -0400

    output management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import './styles/style.css';
 import logo from './images/logo.png';
+import printStuff from './testFunction.js';
 console.log("test");
 
 const content = document.getElementById('content');
 const mainLogo = new Image();
 mainLogo.src = logo;
 content.appendChild(mainLogo);
+const btn = document.createElement('button');
+btn.textContent = "Click me";
+btn.onclick = printStuff;
+content.appendChild(btn);
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-}; 
+    entry: {
+        index: "./src/index.js",
+        testFunction: "./src/testFunction.js",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+};
