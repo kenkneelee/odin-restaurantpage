a89c04b80e9861bec537ab912522e50f3b9aa18b
Kenny Lee <kenny.lee927@gmail.com>
2022-07-18 18:58:09 -0400

    setup htmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
     entry: {
         index: "./src/index.js",
         testFunction: "./src/testFunction.js",
     },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Odin Restaurant Page',
+        template: './src/index.html',
+      }),
+    ],
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
+        clean:true,
     },
     module: {
         rules: [
