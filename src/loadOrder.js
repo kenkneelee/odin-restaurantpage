35b25eb3b626838c7cb1f00fc2e9c7e4b2c522d0
Kenny Lee <kenny.lee927@gmail.com>
2022-07-27 03:46:39 -0400

    dynamically generate locations tab, fix cursor

--- src/loadOrder.js
+++ src/loadOrder.js
@@ -1,28 +1,30 @@
 export default function loadOrder() {
     const content = document.getElementById("content");
-    const orderLink= document.getElementById("link4");
-
+    const orderLink = document.getElementById("link4");
 
     const orderContainer = document.createElement("div");
 
     orderLink.addEventListener("click", () => {
-        content.textContent="";
-        orderContainer.textContent="";
-        orderContainer.id="orderOnline";
+        content.textContent = "";
+        orderContainer.textContent = "";
+        orderContainer.id = "orderOnline";
         const header = document.createElement("h2");
-        header.textContent="Order Online";
+        header.textContent = "Order Online";
         content.appendChild(header);
 
         const orderContent = document.createElement("div");
-        orderContent.id="orderContent";
+        orderContent.id = "orderContent";
         const subHead = document.createElement("h3");
-        subHead.textContent="Order now through one of our partners:"
+        subHead.textContent = "Order now through one of our partners:";
         const uber = document.createElement("img");
-        uber.src = "http://yeoldeorchard.com/wp-content/uploads/2020/04/5310366-uber-eats-logo-png-and-vector-logo-download-uber-eats-png-3500_3500_preview.png"
+        uber.src =
+            "http://yeoldeorchard.com/wp-content/uploads/2020/04/5310366-uber-eats-logo-png-and-vector-logo-download-uber-eats-png-3500_3500_preview.png";
         const doordash = document.createElement("img");
-        doordash.src="https://toppng.com/uploads/preview/doordash-logo-11609359542nd1g660y5t.png"
+        doordash.src =
+            "https://toppng.com/uploads/preview/doordash-logo-11609359542nd1g660y5t.png";
         const skip = document.createElement("img");
-        skip.src="https://pbs.twimg.com/profile_images/1451592207295787014/HG87IRDA_400x400.jpg"
+        skip.src =
+            "https://pbs.twimg.com/profile_images/1451592207295787014/HG87IRDA_400x400.jpg";
         orderContent.appendChild(subHead);
         orderContent.appendChild(uber);
         orderContent.appendChild(doordash);
@@ -32,9 +34,6 @@ export default function loadOrder() {
         // orderContent.appendChild(subHead);
         // orderContent.appendChild(subHead);
 
-
         content.appendChild(orderContainer);
-
-        
-    })
+    });
 }
